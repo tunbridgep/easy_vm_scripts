2cac5629ff2c89413017b3e75666f03ffa2c39da
Paul Tunbridge <tunbridgep@gmail.com>
2020-12-15 00:58:56 +0000

    removed start.sh as it's now in another repo

--- start.sh
+++
@@ -1,7 +0,0 @@
-#download dotfiles
-git clone https://plausiblesarge@bitbucket.org/plausiblesarge/dotfiles.git $1
-
-echo "DOTFILES=$1" >> ~/.profile_vars.local
-
-echo please open the dotfiles folder and run install.sh to get started with a new machine, or run stow.sh to get your dotfiles ready.
-echo \$DOTFILES environment variable added to ~/.profile_vars.local, with value $1
